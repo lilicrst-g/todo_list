d3e726c09e545107e52ee9b1bda6fa9c2058811f
Lisandra <lisa.crisilva@outlook.com>
2026-01-16 12:40:15 -0300

    tela de login

--- lib/app/modules/auth/login/login_page.dart
+++ lib/app/modules/auth/login/login_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_signin_button/flutter_signin_button.dart';
+import 'package:todo_list/app/core/widget/todo_list_logo.dart';
 
 class LoginPage extends StatelessWidget {
   const LoginPage({super.key});
@@ -6,8 +8,104 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Login'),),
-      body: Container(),
+      body: LayoutBuilder(
+        builder: (context, constraints) {
+          return SingleChildScrollView(
+            child: ConstrainedBox(
+              constraints: BoxConstraints(
+                minHeight: constraints.maxHeight,
+                minWidth: constraints.maxWidth
+              ),
+              child: IntrinsicHeight(
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    SizedBox(height: 10,),
+                    TodoListLogo(),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 40, vertical: 20),
+                        child: Form(
+                          child: Column(
+                            children: [
+                              TextFormField(),
+                              SizedBox(height: 20,),
+                              TextFormField(),
+                              SizedBox(height: 10,),
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                children: [
+                                  TextButton(
+                                    onPressed: (){}, 
+                                    child: Text('Esqueceu sua senha?'),
+                                  ),
+                                  ElevatedButton(
+                                    onPressed: (){}, 
+                                    style: ElevatedButton.styleFrom(
+                                      shape: RoundedRectangleBorder(borderRadius:
+                                        BorderRadius.circular(20),
+                                      ),
+                                    ),
+                                    child: Padding(
+                                      padding: const EdgeInsets.all(8.0),
+                                      child: Text('Login'),
+                                    ),
+                                  )
+                                ],
+                              )
+                            ],
+                          ),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    Expanded(
+                      child: Container(
+                        decoration: BoxDecoration(
+                          color: Color(0xffF0F3F7),
+                          border: Border(
+                            top: BorderSide(
+                              width: 2,
+                              color: Colors.grey.withAlpha(50),
+                            ),
+                          ),
+                        ),
+                        child: Column(
+                          children: [
+                            SizedBox(
+                              height: 30,
+                            ),
+                            SignInButton(
+                              Buttons.Google,
+                              text: 'Continue com o Google',
+                              padding: const EdgeInsets.all(5), 
+                              shape: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(30),
+                                borderSide: BorderSide.none,
+                              ),
+                              onPressed: (){},
+                            ),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Text('Não tem conta?'),
+                                TextButton(onPressed: (){}, 
+                                child: Text('Cadastre-se'),
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                    
+                  ],
+                ),
+              ),
+            ),
+          );
+        },
+      ),
     );
   }
 }
