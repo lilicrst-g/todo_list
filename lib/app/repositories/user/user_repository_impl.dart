dd961f8430098d35bc3a31729952e8710ae49528
lilicrst-g <lisa.crisilva@outlook.com>
2026-02-06 15:17:16 -0300

    tela de login integrada

--- lib/app/repositories/user/user_repository_impl.dart
+++ lib/app/repositories/user/user_repository_impl.dart
@@ -1,4 +1,5 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/services.dart';
 import 'package:todo_list/app/exception/auth_exception.dart';
 import 'package:todo_list/app/repositories/user/user_repository.dart';
 
@@ -15,17 +16,40 @@ class UserRepositoryImpl implements UserRepository {
         email: email,
         password: password,
       );
-      return userCredential.user; 
+      return userCredential.user;
     } on FirebaseAuthException catch (e, s) {
       print(e);
       print(s);
       if (e.code == 'email-already-in-use') {
         throw AuthException(
-          message: 'Este e-mail já está em uso. Tente fazer login ou use outro método (Google/Senha).',
+          message:
+              'Este e-mail já está em uso. Tente fazer login ou use outro método (Google/Senha).',
         );
       } else {
         throw AuthException(message: e.message ?? 'Erro ao registrar usuário');
+      }
+    }
   }
+
+  @override
+  Future<User?> login(String email, String password) async {
+    try {
+      final userCredential = await _firebaseAuth.signInWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+      return userCredential.user;
+    } on PlatformException catch (e, s) {
+      print(e);
+      print(s);
+      throw AuthException(message: e.message ?? 'Erro ao realizar login');
+    } on FirebaseAuthException catch (e, s) {
+      print(e);
+      print(s);
+      if (e.code == 'invalid-credential') {
+        throw AuthException(message: "E-mail ou senha inválido");
+      }
+      throw AuthException(message: e.message ?? 'Erro ao realizar login');
     }
   }
 }
